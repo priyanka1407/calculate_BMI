9027aa3f6de634c8229c83ed437f71c46b33ba5e
Priyanka0706 <priyankamurugavel@gmail.com>
2021-10-28 05:49:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#To Calculate BMI Challenge
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+con_height=float(height)
+con_weight=float(weight)
+
+BMI=con_weight/(con_height**2)
+print(int(BMI))
+
+
+
+
+
+
+
+
+
+
